f63e01c98011648747da8dcb7a7eeb2062a29405
ImRoxyRox <rossella.ortu@gmail.com>
2022-07-12 15:11:46 +0100

    second entities push

--- src/main/java/com/qa/dfefinalproject/entities/Fruit.java
+++ src/main/java/com/qa/dfefinalproject/entities/Fruit.java
@@ -1,5 +1,41 @@
 package com.qa.dfefinalproject.entities;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Fruit {
+	
+	private long id;
+	private String kiwi;
+	private String mango;
+	private String pineapple;
+	
+	
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getKiwi() {
+		return kiwi;
+	}
+	public void setKiwi(String kiwi) {
+		this.kiwi = kiwi;
+	}
+	public String getMango() {
+		return mango;
+	}
+	public void setMango(String mango) {
+		this.mango = mango;
+	}
+	public String getPineapple() {
+		return pineapple;
+	}
+	public void setPineapple(String pineapple) {
+		this.pineapple = pineapple;
+	}
 
 }
