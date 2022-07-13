611297a6fcaa120dfe0d1c40cd734473ad7fd6e8
ImRoxyRox <rossella.ortu@gmail.com>
2022-07-13 10:40:02 +0100

    Get=Read

--- src/main/java/com/qa/dfefinalproject/controllers/FruitController.java
+++ src/main/java/com/qa/dfefinalproject/controllers/FruitController.java
@@ -3,6 +3,8 @@ package com.qa.dfefinalproject.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,4 +25,14 @@ public class FruitController {
 		return this.fruits.get(this.fruits.size() - 1);
 	}
 
+	@GetMapping("/readAll")
+	public List<Fruit> readAll() {
+		return this.fruits;
+
+	}
+
+	@GetMapping("/readById/{id}")
+	public Fruit readById(@PathVariable int id) {
+		return this.fruits.get(id);
+	}
 }
