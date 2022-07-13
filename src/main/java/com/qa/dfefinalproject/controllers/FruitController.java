368259c3054eca9487eb83d95a469b17761a39ed
ImRoxyRox <rossella.ortu@gmail.com>
2022-07-13 16:11:57 +0100

    sixt commit

--- src/main/java/com/qa/dfefinalproject/controllers/FruitController.java
+++ src/main/java/com/qa/dfefinalproject/controllers/FruitController.java
@@ -1,6 +1,5 @@
 package com.qa.dfefinalproject.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,42 +11,44 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.dfefinalproject.Services.FruitService;
 import com.qa.dfefinalproject.entities.Fruit;
 
 @RequestMapping("/fruit")
 @RestController
 public class FruitController {
 
-	private List<Fruit> fruits = new ArrayList<>();
+	private FruitService service;
+
+	public FruitController(FruitService service) {
+		this.service = service;
+	}
 
 	@PostMapping("/create")
 	public Fruit create(@RequestBody Fruit fruit) {
-		this.fruits.add(fruit);
+		return this.service.create(fruit);
 
-		return this.fruits.get(this.fruits.size() - 1);
 	}
 
 	@GetMapping("/readAll")
 	public List<Fruit> readAll() {
-		return this.fruits;
+		return this.service.readAll();
 
 	}
 
 	@GetMapping("/readById/{id}")
 	public Fruit readById(@PathVariable int id) {
-		return this.fruits.get(id);
+		return this.service.readById(id);
 	}
 
 	@PutMapping("/update/{id}")
 	public Fruit update(@PathVariable int id, @RequestBody Fruit fruit) {
-		this.fruits.remove(id);
-		this.fruits.add(id, fruit);
-		return this.fruits.get(id);
+		return this.service.update(id, fruit);
 	}
 
-	@DeleteMapping("/delete{id}")
+	@DeleteMapping("/delete/{id}")
 	public Fruit delete(@PathVariable int id) {
-		return this.fruits.remove(id);
+		return this.service.delete(id);
 	}
 
 }
