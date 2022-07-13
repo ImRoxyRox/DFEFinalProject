ac7a7c0bec8c21f8d677fc2aad5cd3e7468658f9
ImRoxyRox <rossella.ortu@gmail.com>
2022-07-13 16:22:42 +0100

    second commit

--- src/main/java/com/qa/dfefinalproject/repos/FruitRepo.java
+++ src/main/java/com/qa/dfefinalproject/repos/FruitRepo.java
@@ -1,8 +1,11 @@
 package com.qa.dfefinalproject.repos;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.qa.dfefinalproject.entities.Fruit;
+
 @Repository
-public class FruitRepo {
+public interface FruitRepo extends JpaRepository<Fruit, Long> {
 
 }
