abf51d092d61e241780b118ae8b31dbd1cfaa47d
ImRoxyRox <rossella.ortu@gmail.com>
2022-07-13 10:38:21 +0100

    Post=Create

--- src/main/java/com/qa/dfefinalproject/controllers/FruitController.java
+++ src/main/java/com/qa/dfefinalproject/controllers/FruitController.java
@@ -1,5 +1,26 @@
 package com.qa.dfefinalproject.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.dfefinalproject.entities.Fruit;
+
+@RequestMapping("/fruit")
+@RestController
 public class FruitController {
+
+	private List<Fruit> fruits = new ArrayList<>();
+
+	@PostMapping("/create")
+	public Fruit create(@RequestBody Fruit fruit) {
+		this.fruits.add(fruit);
+
+		return this.fruits.get(this.fruits.size() - 1);
+	}
 
 }
