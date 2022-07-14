4af1f8d30066df922484d05102389b04b2cbdad7
ImRoxyRox <rossella.ortu@gmail.com>
2022-07-14 15:06:31 +0100

    second commit

--- src/test/java/com/qa/dfefinalproject/controllers/FruitControllerTest.java
+++ src/test/java/com/qa/dfefinalproject/controllers/FruitControllerTest.java
@@ -1,5 +1,12 @@
 package com.qa.dfefinalproject.controllers;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class FruitControllerTest {
 
 }
