3cc844974e28310006b0be105f07697fc3a6caed
ImRoxyRox <rossella.ortu@gmail.com>
2022-07-14 09:26:17 +0100

    Last push

--- src/main/java/com/qa/dfefinalproject/controllers/FruitController.java
+++ src/main/java/com/qa/dfefinalproject/controllers/FruitController.java
@@ -27,28 +27,25 @@ public class FruitController {
 	@PostMapping("/create")
 	public Fruit create(@RequestBody Fruit fruit) {
 		return this.service.create(fruit);
-
 	}
 
 	@GetMapping("/readAll")
 	public List<Fruit> readAll() {
 		return this.service.readAll();
-
 	}
 
 	@GetMapping("/readById/{id}")
-	public Fruit readById(@PathVariable int id) {
+	public Fruit readById(@PathVariable long id) {
 		return this.service.readById(id);
 	}
 
 	@PutMapping("/update/{id}")
-	public Fruit update(@PathVariable int id, @RequestBody Fruit fruit) {
+	public Fruit update(@PathVariable long id, @RequestBody Fruit fruit) {
 		return this.service.update(id, fruit);
 	}
 
-	@DeleteMapping("/delete/{id}")
-	public Fruit delete(@PathVariable int id) {
+	@DeleteMapping("/delete{id}")
+	public boolean delete(@PathVariable long id) {
 		return this.service.delete(id);
 	}
-
 }
