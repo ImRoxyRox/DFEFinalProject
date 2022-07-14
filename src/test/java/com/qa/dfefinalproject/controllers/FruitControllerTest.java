1fadfd72bfe11e572462b774e6d4b2db6693b1fe
ImRoxyRox <rossella.ortu@gmail.com>
2022-07-14 21:25:02 +0100

    commit

--- src/test/java/com/qa/dfefinalproject/controllers/FruitControllerTest.java
+++ src/test/java/com/qa/dfefinalproject/controllers/FruitControllerTest.java
@@ -18,13 +18,14 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.servlet.MockMvc;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qa.dfefinalproject.entities.Fruit;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Sql(scripts = { "classpath:testschema.sql",
-		"classpath:testdate.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+		"classpath:testdata.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 @ActiveProfiles("test")
 public class FruitControllerTest {
 
@@ -33,18 +34,16 @@ public class FruitControllerTest {
 
 	@Autowired
 	private ObjectMapper mapper;
-	
+
 	@Test
 	public void createTest() throws Exception {
 		Fruit entry = new Fruit("Hardy", "Francis", "Queen");
 		String entryAsJSON = mapper.writeValueAsString(entry);
-		
+
 		Fruit result = new Fruit(2L, "Hardy", "Francis", "Queen");
 		String resultAsJSON = mapper.writeValueAsString(result);
-		
-		mvc.perform(post("/fruit/create")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(entryAsJSON))
+
+		mvc.perform(post("/fruit/create").contentType(MediaType.APPLICATION_JSON).content(entryAsJSON))
 				.andExpect(content().json(resultAsJSON));
 	}
 
@@ -57,18 +56,16 @@ public class FruitControllerTest {
 
 		String outputAsJSON = mapper.writeValueAsString(output);
 
-		mvc.perform(get("/fruit/readAll")
-				.contentType(MediaType.APPLICATION_JSON))
+		mvc.perform(get("/fruit/readAll").contentType(MediaType.APPLICATION_JSON))
 				.andExpect(content().json(outputAsJSON));
 	}
-	
+
 	@Test
 	public void readByIdTest() throws Exception {
 		Fruit entry = new Fruit(1L, "Actinidia", "Ataulfo", "Abacaxi");
 		String entryAsJSON = this.mapper.writeValueAsString(entry);
-		
-		mvc.perform(get("/fruit/readById/1")
-				.contentType(MediaType.APPLICATION_JSON))
+
+		mvc.perform(get("/fruit/readById/1").contentType(MediaType.APPLICATION_JSON))
 				.andExpect(content().json(entryAsJSON));
 	}
 
@@ -78,18 +75,14 @@ public class FruitControllerTest {
 		Fruit result = new Fruit(1L, "Hardy", "Francis", "Queen");
 		String entryAsJSON = this.mapper.writeValueAsString(entry);
 		String resultAsJSON = this.mapper.writeValueAsString(result);
-		
-		mvc.perform(put("/fruit/update/1")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(entryAsJSON))
-		.andExpect(content().json(resultAsJSON));
+
+		mvc.perform(put("/fruit/update/1").contentType(MediaType.APPLICATION_JSON).content(entryAsJSON))
+				.andExpect(content().json(resultAsJSON));
 	}
-	
+
 	@Test
 	public void deleteTest() throws Exception {
-		mvc.perform(delete("/fruit/delete/1")
-				.contentType(MediaType.APPLICATION_JSON))
-		.andExpect(content().string("true"));
-		
+		mvc.perform(delete("/fruit/delete/1").contentType(MediaType.APPLICATION_JSON))
+				.andExpect(content().string("true"));
 	}
 }
