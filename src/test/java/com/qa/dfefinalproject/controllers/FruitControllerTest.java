e5d237f01ac870d713af750eedc7001738a04cc3
ImRoxyRox <rossella.ortu@gmail.com>
2022-07-14 16:37:31 +0100

    third push

--- src/test/java/com/qa/dfefinalproject/controllers/FruitControllerTest.java
+++ src/test/java/com/qa/dfefinalproject/controllers/FruitControllerTest.java
@@ -1,12 +1,48 @@
 package com.qa.dfefinalproject.controllers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.dfefinalproject.entities.Fruit;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Sql(scripts = { "classpath:testschema.sql",
+		"classpath:testdate.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 @ActiveProfiles("test")
 public class FruitControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Autowired
+	private ObjectMapper mapper;
+
+	@Test
+	public void readAllTest() throws Exception {
+
+		List<Fruit> output = new ArrayList<>();
+		Fruit entry = new Fruit(1L, "Kiwi", "Mango", "Pineapple");
+		output.add(entry);
+
+		String outputAsJSON = mapper.writeValueAsString(output);
+
+		mvc.perform(get("/fruit/readAll").contentType(MediaType.APPLICATION_JSON))
+				.andExpect(content().json(outputAsJSON));
+	}
 
 }
